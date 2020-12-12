a0ffffa15409227559709b4b41e1064f36d8e237
Navneet <Navneet.Dabral@ril.com>
2020-12-12 18:26:59 +0530

    basic template added

---
+++ src/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
